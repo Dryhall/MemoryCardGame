82f0b37aca12e035c196987ec06afca5897efb65
Lakksh Sharma <112651654+Dryhall@users.noreply.github.com>
2023-11-21 06:41:08 +0000

    Initial commit.

---
+++ src/memorycardgame_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { memorycardgame_backend } from "../../declarations/memorycardgame_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await memorycardgame_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
